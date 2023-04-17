329f64f07894c9fc96476835789339a1eb83bc35
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-04-17 09:23:35 +0530

    Add files via upload

---
+++ Indexer/Indexer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            indexer i;
+            i = new indexer();
+            i[3] = 4.3f;
+            Console.WriteLine(i[3]);
+        }
+    }
+}
